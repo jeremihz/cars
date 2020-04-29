b3435bcdae0c02a9993388bade12db92259128f0
jeremihz <mihjere68@gmail.com>
2020-04-29 15:09:36 +0300

    first commit

---
+++ database/car2.sql
@@ -0,0 +1,191 @@
+-- phpMyAdmin SQL Dump
+-- version 4.8.5
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1:3306
+-- Generation Time: Apr 29, 2020 at 12:05 PM
+-- Server version: 5.7.26
+-- PHP Version: 7.1.29
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET AUTOCOMMIT = 0;
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `car2`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `cars`
+--
+
+DROP TABLE IF EXISTS `cars`;
+CREATE TABLE IF NOT EXISTS `cars` (
+  `car_id` int(20) NOT NULL AUTO_INCREMENT,
+  `car_name` varchar(50) NOT NULL,
+  `car_nameplate` varchar(50) NOT NULL,
+  `car_img` varchar(50) DEFAULT 'NA',
+  `ac_price` float NOT NULL,
+  `non_ac_price` float NOT NULL,
+  `ac_price_per_day` float NOT NULL,
+  `non_ac_price_per_day` float NOT NULL,
+  `car_availability` varchar(10) NOT NULL,
+  PRIMARY KEY (`car_id`),
+  UNIQUE KEY `car_nameplate` (`car_nameplate`)
+) ENGINE=InnoDB AUTO_INCREMENT=15 DEFAULT CHARSET=utf8;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `clientcars`
+--
+
+DROP TABLE IF EXISTS `clientcars`;
+CREATE TABLE IF NOT EXISTS `clientcars` (
+  `car_id` int(20) NOT NULL,
+  `client_username` varchar(50) NOT NULL,
+  PRIMARY KEY (`car_id`),
+  KEY `client_username` (`client_username`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `clients`
+--
+
+DROP TABLE IF EXISTS `clients`;
+CREATE TABLE IF NOT EXISTS `clients` (
+  `client_username` varchar(50) NOT NULL,
+  `client_name` varchar(50) NOT NULL,
+  `client_phone` varchar(15) NOT NULL,
+  `client_email` varchar(25) NOT NULL,
+  `client_address` varchar(50) CHARACTER SET utf8 COLLATE utf8_estonian_ci NOT NULL,
+  `client_password` varchar(20) NOT NULL,
+  PRIMARY KEY (`client_username`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `customers`
+--
+
+DROP TABLE IF EXISTS `customers`;
+CREATE TABLE IF NOT EXISTS `customers` (
+  `customer_username` varchar(50) NOT NULL,
+  `customer_name` varchar(50) NOT NULL,
+  `customer_phone` varchar(15) NOT NULL,
+  `customer_email` varchar(25) NOT NULL,
+  `customer_address` varchar(50) NOT NULL,
+  `customer_password` varchar(20) NOT NULL,
+  PRIMARY KEY (`customer_username`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `driver`
+--
+
+DROP TABLE IF EXISTS `driver`;
+CREATE TABLE IF NOT EXISTS `driver` (
+  `driver_id` int(20) NOT NULL AUTO_INCREMENT,
+  `driver_name` varchar(50) NOT NULL,
+  `dl_number` varchar(50) NOT NULL,
+  `driver_phone` varchar(15) NOT NULL,
+  `driver_address` varchar(50) NOT NULL,
+  `driver_gender` varchar(10) NOT NULL,
+  `client_username` varchar(50) NOT NULL,
+  `driver_availability` varchar(10) NOT NULL,
+  PRIMARY KEY (`driver_id`),
+  UNIQUE KEY `dl_number` (`dl_number`),
+  KEY `client_username` (`client_username`)
+) ENGINE=InnoDB AUTO_INCREMENT=9 DEFAULT CHARSET=utf8;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `feedback`
+--
+
+DROP TABLE IF EXISTS `feedback`;
+CREATE TABLE IF NOT EXISTS `feedback` (
+  `name` varchar(20) NOT NULL,
+  `e_mail` varchar(30) NOT NULL,
+  `message` varchar(150) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Dumping data for table `feedback`
+--
+
+INSERT INTO `feedback` (`name`, `e_mail`, `message`) VALUES
+('Nikhil', 'nikhil@gmail.com', 'Hope this works.');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `rentedcars`
+--
+
+DROP TABLE IF EXISTS `rentedcars`;
+CREATE TABLE IF NOT EXISTS `rentedcars` (
+  `id` int(100) NOT NULL AUTO_INCREMENT,
+  `customer_username` varchar(50) NOT NULL,
+  `car_id` int(20) NOT NULL,
+  `driver_id` int(20) NOT NULL,
+  `booking_date` date NOT NULL,
+  `rent_start_date` date NOT NULL,
+  `rent_end_date` date NOT NULL,
+  `car_return_date` date DEFAULT NULL,
+  `fare` double NOT NULL,
+  `charge_type` varchar(25) NOT NULL DEFAULT 'days',
+  `distance` double DEFAULT NULL,
+  `no_of_days` int(50) DEFAULT NULL,
+  `total_amount` double DEFAULT NULL,
+  `return_status` varchar(10) NOT NULL,
+  PRIMARY KEY (`id`),
+  KEY `customer_username` (`customer_username`),
+  KEY `car_id` (`car_id`),
+  KEY `driver_id` (`driver_id`)
+) ENGINE=InnoDB AUTO_INCREMENT=574681262 DEFAULT CHARSET=utf8;
+
+--
+-- Constraints for dumped tables
+--
+
+--
+-- Constraints for table `clientcars`
+--
+ALTER TABLE `clientcars`
+  ADD CONSTRAINT `clientcars_ibfk_1` FOREIGN KEY (`client_username`) REFERENCES `clients` (`client_username`),
+  ADD CONSTRAINT `clientcars_ibfk_2` FOREIGN KEY (`car_id`) REFERENCES `cars` (`car_id`);
+
+--
+-- Constraints for table `driver`
+--
+ALTER TABLE `driver`
+  ADD CONSTRAINT `driver_ibfk_1` FOREIGN KEY (`client_username`) REFERENCES `clients` (`client_username`);
+
+--
+-- Constraints for table `rentedcars`
+--
+ALTER TABLE `rentedcars`
+  ADD CONSTRAINT `rentedcars_ibfk_1` FOREIGN KEY (`customer_username`) REFERENCES `customers` (`customer_username`),
+  ADD CONSTRAINT `rentedcars_ibfk_2` FOREIGN KEY (`car_id`) REFERENCES `cars` (`car_id`),
+  ADD CONSTRAINT `rentedcars_ibfk_3` FOREIGN KEY (`driver_id`) REFERENCES `driver` (`driver_id`);
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
